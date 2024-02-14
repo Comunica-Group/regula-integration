28c2b7f9fb89f6de079df81a0e3a69c7f38ff1a6
estebansamayoa1 <61565155+estebansamayoa1@users.noreply.github.com>
2024-02-14 10:09:47 -0600

    Arreglado frontend e info

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { initialize } from './actions';
-import logo from './logo.svg';
+import comunica from './Comunica.png';
 import './app.css';
 import { defineComponents, DocumentReaderService } from '@regulaforensics/vp-frontend-document-components';
-import { DocumentReaderApi, Result, Scenario, Source, TextFieldType, GraphicFieldType, Light,SecurityFeatureType } from '@regulaforensics/document-reader-webclient';
+import { DocumentReaderApi, Result, Scenario, Source, TextFieldType, GraphicFieldType, Light, SecurityFeatureType } from '@regulaforensics/document-reader-webclient';
 
 const { PORTRAIT, DOCUMENT_FRONT } = GraphicFieldType;
 const { DOCUMENT_NUMBER, GIVEN_NAMES, SURNAME } = TextFieldType;
@@ -13,6 +13,26 @@ const regula_license = process.env.REACT_APP_REGULA_LICENSE;
 const apiBasePath = process.env.REACT_APP_BASE_PATH;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      serverVersion: null,
+      docOverallStatus: null,
+      docNumberVisual: null,
+      Name: null,
+      LastName: null,
+      docNumberMrz: null,
+      docNumberVisualValidity: null,
+      docNumberMrzValidity: null,
+      docNumberMrzVisualMatching: null,
+      documentImageBase64: null,
+      portraitImageBase64: null,
+      activeTab: 'DocumentReader' // Default active tab
+    };
+    // Bind the listener function
+    this.listener = this.listener.bind(this);
+  }
+
   componentDidMount() {
     this.props.initialize();
 
@@ -21,60 +41,79 @@ class App extends Component {
 
     const component = document.querySelector('document-reader');
 
-    async function listener(event) {
-      if (event.detail.action === 'PROCESS_FINISHED' && event.detail.data.status === 1) {
-        const response = event.detail.data.response;
-        const image_container = response.lowLvlResponse.ContainerList.List[0].RawImageContainer;
-        const image_format = image_container.format;
-        const image = image_container.image;
-        const api = new DocumentReaderApi({ basePath: apiBasePath });
+    // Use the bound function
+    component.addEventListener('document-reader', this.listener);
 
-        api.setLicense(regula_license);
-        const serverInfo = await api.ping();
-        const request = {
-          images: [
-              {
-                  ImageData: image,
-                  light: Light.WHITE,
-                  page_idx: 0,
-              },
-          ],
-          processParam: {
-              scenario: Scenario.FULL_AUTH,
-              resultTypeOutput: [
-                  // actual results
-                  Result.STATUS,
-                  Result.AUTHENTICITY,
-                  Result.TEXT,
-                  Result.IMAGES,
-                  Result.DOCUMENT_TYPE,
-                  Result.DOCUMENT_TYPE_CANDIDATES,
-                  Result.IMAGE_QUALITY,
-                  // legacy results
-                  Result.MRZ_TEXT,
-                  Result.VISUAL_TEXT,
-                  Result.BARCODE_TEXT,
-                  Result.RFID_TEXT,
-                  Result.VISUAL_GRAPHICS,
-                  Result.BARCODE_GRAPHICS,
-                  Result.RFID_GRAPHICS,
-                  Result.LEXICAL_ANALYSIS,
-              ],
+    window.RegulaDocumentSDK.recognizerProcessParam = {
+      processParam: {
+        returnUncroppedImage: true,
+        scenario: 'MrzAndLocate',
+        multipageProcessing: true,
+        returnPackageForReprocess: false,
+        timeout: 20000,
+        resultTypeOutput: ['TextDataResult'],
+      },
+    };
+  }
+
+  componentWillUnmount() {
+    const component = document.querySelector('document-reader');
+    if (component) {
+      component.removeEventListener('document-reader', this.listener);
+    }
+  }
+
+  async listener(event) {
+    console.log(event);
+    if (event.detail.action === 'PROCESS_FINISHED' && event.detail.data.status === 1) {
+      const response = event.detail.data.response;
+      console.log('Response:', response);
+      const image_container = response.lowLvlResponse.ContainerList.List[0].RawImageContainer;
+      const image_format = image_container.format;
+      const image = image_container.image;
+      const api = new DocumentReaderApi({ basePath: apiBasePath });
+
+      api.setLicense(regula_license);
+      const serverInfo = await api.ping();
+      const request = {
+        images: [
+          {
+            ImageData: image,
+            light: Light.WHITE,
+            page_idx: 0,
           },
+        ],
+        processParam: {
+          scenario: Scenario.FULL_AUTH,
+          resultTypeOutput: [
+            // actual results
+            Result.STATUS,
+            Result.AUTHENTICITY,
+            Result.TEXT,
+            Result.IMAGES,
+            Result.DOCUMENT_TYPE,
+            Result.DOCUMENT_TYPE_CANDIDATES,
+            Result.IMAGE_QUALITY,
+            // legacy results
+            Result.MRZ_TEXT,
+            Result.VISUAL_TEXT,
+            Result.BARCODE_TEXT,
+            Result.RFID_TEXT,
+            Result.VISUAL_GRAPHICS,
+            Result.BARCODE_GRAPHICS,
+            Result.RFID_GRAPHICS,
+            Result.LEXICAL_ANALYSIS,
+          ],
+        },
       };
 
       const responseProcessed = await api.process(request);
-      const requestJson = JSON.stringify(request);
-      const responseJson = responseProcessed.json();
 
       const docOverallStatus = responseProcessed.status.overallStatus;
-      const docOpticalTextStatus = responseProcessed.status.detailsOptical.text;
 
       const docNumberField = responseProcessed.text.getField(DOCUMENT_NUMBER);
       const docNameField = responseProcessed.text.getField(GIVEN_NAMES);
-      const docNumberFieldByName = responseProcessed.text.getFieldByName('Document Number');
-      const docNamesField = responseProcessed.text.getFieldByName('Given Names');
-      const docLastNamesField = responseProcessed.text.getFieldByName('Surname');
+      const docLastNamesField = responseProcessed.text.getField(SURNAME);
 
       const docNumberVisual = docNumberField.getValue(Source.VISUAL);
       const Name = docNameField.getValue(Source.TEXT);
@@ -86,81 +125,149 @@ class App extends Component {
 
       const docAuthenticity = responseProcessed.authenticity();
 
-      const docImagePattern = docAuthenticity.imagePatternChecks();
-      const docImagePatternBlankChecks = docImagePattern.checksByElement(SecurityFeatureType.BLANK);
-
-      const documentImage = responseProcessed.images.getField(DOCUMENT_FRONT).getValue();
+      const documentImage = responseProcessed.images.getField(DOCUMENT_FRONT);
       const portraitField = responseProcessed.images.getField(PORTRAIT);
-      const portraitFromVisual = portraitField.getValue(Source.VISUAL);
-      // Here you should handle saving files, possibly using browser APIs
-      console.log('Document Image:', documentImage);
-      console.log('Portrait:', portraitFromVisual);
 
-      const docImageQuality = responseProcessed.imageQualityChecks();
+      const valuedocument_base64 = documentImage.valueList[0].value;
+      const valueportrait_base64 = portraitField.valueList[0].value;
 
-      console.log('-----------------------------------------------------------------');
-      console.log(`            Web API version: ${serverInfo.version}`);
-      console.log('-----------------------------------------------------------------');
-      console.log(`           Document Overall Status: ${docOverallStatus}`);
-      console.log(`            Document Number Visual: ${docNumberVisual}`);
-      console.log(`            Given Name: ${Name}`);
-      console.log(`            Last Name: ${LastName}`);
-      console.log(`               Document Number MRZ: ${docNumberMrz}`);
-      console.log(`Validity Of Document Number Visual: ${docNumberVisualValidity}`);
-      console.log(`   Validity Of Document Number MRZ: ${docNumberMrzValidity}`);
-      console.log(`      MRZ-Visual values comparison: ${docNumberMrzVisualMatching}`);
-      console.log('-----------------------------------------------------------------');
-
-      }
+      this.setState({
+        serverVersion: serverInfo.version,
+        docOverallStatus,
+        docNumberVisual,
+        Name,
+        LastName,
+        docNumberMrz,
+        docNumberVisualValidity,
+        docNumberMrzValidity,
+        docNumberMrzVisualMatching,
+        documentImageBase64: valuedocument_base64,
+        portraitImageBase64: valueportrait_base64
+      });
+    } else {
+      // Reset state when a new request is triggered
+      this.setState({
+        docOverallStatus: null,
+        docNumberVisual: null,
+        Name: null,
+        LastName: null,
+        docNumberMrz: null,
+        docNumberVisualValidity: null,
+        docNumberMrzValidity: null,
+        docNumberMrzVisualMatching: null,
+        documentImageBase64: null,
+        portraitImageBase64: null
+      });
     }
-
-    // Add the event listener to the 'document-reader' element
-    component.addEventListener('document-reader', listener);
-
-    // Add the recognizerProcessParam to the RegulaDocumentSDK
-    window.RegulaDocumentSDK.recognizerProcessParam = {
-      processParam: {
-        returnUncroppedImage: true,
-        scenario: 'MrzAndLocate',
-        multipageProcessing: false,
-        returnPackageForReprocess: false,
-        timeout: 20000,
-        resultTypeOutput: ['TextDataResult'],
-        imageQa: {
-          expectedPass: ['dpiThreshold', 'glaresCheck', 'focusCheck'],
-          dpiThreshold: 130,
-          glaresCheck: true,
-          glaresCheckParams: {
-            imgMarginPart: 0.05,
-            maxGlaringPart: 0.01,
-          },
-        },
-      },
-    };
   }
+
+  componentWillUnmount() {
+    const component = document.querySelector('document-reader');
+    if (component) {
+      component.removeEventListener('document-reader', this.listener);
+    }
+  }
+
+
+  renderDocumentReaderTab = () => {
+    return (
+      <div>
+        <document-reader start-screen></document-reader>
+        {this.state.serverVersion && (
+          <div className="document-info-container">
+            <h2>Document Information</h2>
+            <div className="document-info">
+              <div>
+                <h5>Web API version:</h5>
+                <p>{this.state.serverVersion}</p>
+              </div>
+              <div>
+                <h5>Document Overall Status:</h5>
+                <p>{this.state.docOverallStatus}</p>
+              </div>
+              <div>
+                <h5>Document Number Visual:</h5>
+                <p>{this.state.docNumberVisual}</p>
+              </div>
+              <div>
+                <h5>Given Name:</h5>
+                <p>{this.state.Name}</p>
+              </div>
+              <div>
+                <h5>Last Name:</h5>
+                <p>{this.state.LastName}</p>
+              </div>
+              <div>
+                <h5>Document Number MRZ:</h5>
+                <p>{this.state.docNumberMrz}</p>
+              </div>
+              <div>
+                <h5>Validity Of Document Number Visual:</h5>
+                <p>{this.state.docNumberVisualValidity}</p>
+              </div>
+              <div>
+                <h5>Validity Of Document Number MRZ:</h5>
+                <p>{this.state.docNumberMrzValidity}</p>
+              </div>
+              <div>
+                <h5>MRZ-Visual values comparison:</h5>
+                <p>{this.state.docNumberMrzVisualMatching}</p>
+              </div>
+              {this.state.documentImageBase64 && (
+                <div>
+                  <h5>Document Image:</h5>
+                  <img src={`data:image/png;base64,${this.state.documentImageBase64}`} alt="Document" />
+                </div>
+              )}
+              {this.state.portraitImageBase64 && (
+                <div>
+                  <div><br /></div>
+                  <h5>Portrait Image:</h5>
+                  <img src={`data:image/png;base64,${this.state.portraitImageBase64}`} alt="Document" />
+                </div>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  };
+
+  renderFaceSDKTab = () => {
+    return (
+    <div>
+    <face-capture></face-capture>
+    <script type="module" src="index.js"></script>
+    </div>
+    );
+  };
 
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Comunica Regula Integration</h2>
+          <img className="App-logo" src={comunica} alt="Logo" />
+          <h1>ID Verification Demo</h1>
         </div>
-        <document-reader start-screen></document-reader>
+        <div className="Tabs">
+          <button onClick={() => this.setState({ activeTab: 'DocumentReader' })}>Document Reader</button>
+          <button onClick={() => this.setState({ activeTab: 'FaceSDK' })}>Face SDK</button>
+        </div>
+        {this.state.activeTab === 'DocumentReader' && this.renderDocumentReaderTab()}
+        {this.state.activeTab === 'FaceSDK' && this.renderFaceSDKTab()}
+        <footer className="Footer">
+          <img className="Footer-logo" src={comunica} alt="Logo" />
+        </footer>
       </div>
     );
   }
 }
+
 
 function mapStateToProps(state) {
   return state;
 }
 
 export default connect(mapStateToProps, { initialize })(App);
-
-
-
-
-
-
 
 
